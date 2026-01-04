186853a267a2266ccdb8fbd55576a2259134594b
Xeron23 <kaqyword2304@example.com>
2026-01-04 16:52:15 +0700

    fix: length validasi createGymSchema

--- src/domains/gym/gym.schema.js
+++ src/domains/gym/gym.schema.js
@@ -59,7 +59,7 @@ const createGymSchema = Joi.object({
             "string.min": "Address must be at least 4 characters long.",
             "string.base": "Username can only contain letters and spaces."
     }),
-    jamOperasional: Joi.string().min(4).max(150).required()
+    jamOperasional: Joi.string().min(4).max(170).required()
         .messages({
             "string.empty": "Jam Operational is required.",
             "string.min": "jam Operational be at least 4 characters long.",
